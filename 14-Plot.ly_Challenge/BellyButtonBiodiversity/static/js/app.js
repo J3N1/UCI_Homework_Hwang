960eb53df1314050f1d592397bbb0ae6427dc028
J3N1 <hwang.jenifer@gmail.com>
2021-01-03 16:16:43 -0800

    bubble

--- 14-Plot.ly_Challenge/BellyButtonBiodiversity/static/js/app.js
+++ 14-Plot.ly_Challenge/BellyButtonBiodiversity/static/js/app.js
@@ -10,5 +10,73 @@ function getPlot(id){
         //filter sample values by id
         var samples = data.samples.filter(s => s.id.toString() === id)[0];
         console.log(samples);
+
+        //Getting top 10
+        var samplevalues = samples.sample_values.slice(0,10).reverse();
+
+        //get only top 10 otu ids for the plot
+        var OTU_top = (samples.otu_ids.slice(0,10)).reverse();
+        //get the otu ids for the plot
+        var OTU_id = OTU_top.map(d => "OTU" + d);
+
+        //get the top 10 labels for the plot
+        var labels = samples.otu_labels.slice(0,10);
+
+        //create trace vairable for the plot 
+        var trace = {
+            x: samplevalues,
+            y: OTU_id,
+            text: labels, 
+            marker: {
+                color: 'rgb(142,124,195)',
+            },
+            type: "bar",
+            orientation: "h"
+        };
+
+        //create data variable
+        var data = [trace];
+
+        //create layout variable to set plots layout
+        var lyout = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode: "linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+        };
+
+        //create the bar plot
+        Plotly.newPlot("bar", data, layout);
+
+        //The bubble chart
+        var trace1 = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+        };
+
+        //set the layout for the bubble plot
+        var layout_b = {
+            xaxis: {title: "OTU ID"},
+            height: 600,
+            width: 1000
+        };
+
+        //creating data variable
+        var data1 = [trace1];
+
+        //create bubble plot
+        Plotly.newPlot("bubble", data1, layout_b);
     });
 };
